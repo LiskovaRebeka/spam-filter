4a922760864edea9a149e6fecc51f29e4669af6b
LiskovaRebeka <75761991+LiskovaRebeka@users.noreply.github.com>
2020-12-17 00:16:45 +0100

    Update filter.py

--- filter.py
+++ filter.py
@@ -23,8 +23,7 @@ class BaseFilter():
 class BayesFilter(BaseFilter):
     def train(self, training_dir):
         training_corpus = corpus.TrainingCorpus(training_dir)
-        trained_corpus_dict = training_corpus.get_overall_spammicity()
-        training_word_counter = training_corpus.separate_words_from_emails()
+        self.trained_corpus_dict = training_corpus.get_overall_spammicity()
 
     def test(self, dir_path):
         # TODO:
@@ -33,8 +32,35 @@ class BayesFilter(BaseFilter):
         # - problem with nonexistent words
         # - solutions for similiar words (eg. those with extra "!")
         # -- maybe look for subsets of words
-        resulting_dict = self.classify_emails(dir_path)
+
+        testing_corpus = corpus.TrainingCorpus(dir_path)
+        for file_name, file_body in testing_corpus.emails():
+            testing_corpus_array = testing_corpus.separate_words_from_emails(file_name, file_body)
+            words_in_email = []
+            testing_word_counter = testing_corpus.count_words_from_emails()
+            
+            for i in range(len(testing_corpus_array)):
+                words_in_email.append(testing_corpus_array[i])
+
+            words_with_values = self.value_for_words_in_email(words_in_email)
+            
+
+
+        #resulting_dict = self.classify_emails(dir_path)
         # TODO: write the final evaluation to a file
+
+    def value_for_words_in_email(self, words_in_email):
+        dictionary = {}
+        for word in words_in_email:
+            if word in self.trained_corpus_dict:
+                dictionary[word] = self.trained_corpus_dict[word]
+            else:
+                # Which value is best for word
+                # that was not in the training corpus
+                # Now it is set on 0.4
+                dictionary[word] = 0.4
+        return(dictionary)
+
 
     def classify_emails(self, dir_path):
         testing_corpus = corpus.TrainingCorpus(dir_path)
