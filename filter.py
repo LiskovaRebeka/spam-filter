4d9b6f88efab65de0451b123e93c134723e9f837
LiskovaRebeka <75761991+LiskovaRebeka@users.noreply.github.com>
2020-12-19 03:37:30 +0100

    Update filter.py

--- filter.py
+++ filter.py
@@ -2,25 +2,26 @@ import corpus
 import os
 import utils
 
+
 from collections import Counter
 
 SPAM_TAG = "SPAM"
-HAM_TAG = "HAM"
+HAM_TAG = "OK"
 SPAM_VALUE_FOR_NEW_WORD = 0.4
-SPAM_PROBABILITY_TOLERANCE = 0.8
+SPAM_PROBABILITY_TOLERANCE = 0.9
 
 
 class BaseFilter():
-    # Parent class for all possible filter types
-    def test(self, testing_dir):
+    # Parent class for all possible filter types 
+    def test(self, dir_path):
         result_dict = {}
-        files = os.listdir(testing_dir)
+        files = os.listdir(dir_path)
         for email in files:
             if email[0] != '!':
                 # TODO: function classify.email()
                 result_dict[email] = self.classify_email()
         utils.write_classification_to_file(
-                    os.path.join(testing_dir, '!prediction.txt'), result_dict)
+                    os.path.join(dir_path, '!prediction.txt'), result_dict)
 
     def classify_email(self):
         temporary_evaluation = "OK"
@@ -59,16 +60,16 @@ class BayesFilter(BaseFilter):
                 # a bit awkward way to remove words with those weights 0, 1
                 if complete_counter[word] != 0 and complete_counter[word] != 1:
                     # TODO:
-                    # I am not really sure about the order of operations here
+                    # (I am not really sure about the order of operations here)
+                    # I am not sure either
                     numerator *= complete_counter[word]
-                    denominator += numerator
                     denominator *= (1 - complete_counter[word])
                 words_considered += 1
                 # TODO: Consider words with the most important weights
                 # Now first fifteen words are considered
                 if (words_considered > 15):
                     break
-            predicted_spam_probability[email] = numerator / denominator
+            predicted_spam_probability[email] = numerator / (numerator + denominator)
 
         self.write_results_to_file(predicted_spam_probability, testing_dir)
 
@@ -76,14 +77,17 @@ class BayesFilter(BaseFilter):
         # fill the counter with values of words not seen in training phase
         complete_counter = Counter()
         for word in words_in_email:
-            # Word was found in training dataset
-            if word in self.trained_corpus_dict:
-                complete_counter[word] = self.trained_corpus_dict[word]
-            # word is new
-            else:
-                # Which value is best for word
-                # that was not in the training corpus
-                # Now it is set on 0.4
+            # training dataset exists and word was found there
+            try:
+                if word in self.trained_corpus_dict:
+                    complete_counter[word] = self.trained_corpus_dict[word]
+                # word is new
+                else:
+                    # Which value is best for word
+                    # that was not in the training corpus
+                    # Now it is set on 0.4
+                    complete_counter[word] = SPAM_VALUE_FOR_NEW_WORD
+            except AttributeError:
                 complete_counter[word] = SPAM_VALUE_FOR_NEW_WORD
         return complete_counter
 
